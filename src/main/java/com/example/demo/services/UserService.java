3f295caddddb30c93b84ed4c43530df0272f702d
Adeyemo Adeolu <adeyemo.sunday@konga.com>
2021-03-17 16:09:26 +0100

    Implement get user by Id

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +15,13 @@ public class UserService {
 
     public List<User> getAllUser(){
         return userRepositories.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepositories.save(user);
+    }
+
+    public Optional<User> getUserById(Long id){
+        return userRepositories.findById(id);
     }
 }
