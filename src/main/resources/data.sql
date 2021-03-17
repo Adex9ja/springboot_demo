483e37de6b664ed184c01c2ba18b269da4adbb6b
Adeyemo Adeolu <adeyemo.sunday@konga.com>
2021-03-17 15:19:10 +0100

    adding repositories

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values (101, 'kennedy@gmail.com', 'kalyn', 'Kennedy', 'admin', 'ssn001', 'ade')
+insert into user values (102, 'adex9ja@gmail.com', 'kalyn', 'Kennedy', 'admin', 'ssn002', 'olu')
+insert into user values (103, 'yemi@gmail.com', 'kalyn', 'Kennedy', 'admin', 'ssn003', 'sunday')
