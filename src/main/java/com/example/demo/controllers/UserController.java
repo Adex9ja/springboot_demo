9d7303f3d51aec81b069103c89af43540faaca1b
Adeyemo Adeolu <adeyemo.sunday@konga.com>
2021-03-17 15:38:43 +0100

    implements the controller

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getAllUser(){
+        return userService.getAllUser();
+    }
+}
